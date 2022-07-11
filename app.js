71f25ab33e9493218c32e1870c3f39fb6fb31ab7
SebastianOrkiszewski <s.orkiszewski@gmail.com>
2022-07-11 21:21:23 +0200

    add gellery to popup

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const THUMBNAIL = document.querySelectorAll('.thumbnail img');
 const POPUP = document.querySelector('.popup');
 const POPUP_CLOSE = document.querySelector('.popup__close');
+const POPUP_IMG = document.querySelector('.popup__img');
 
 THUMBNAIL.forEach((thumbnail) => {
-    thumbnail.addEventListener('click', () => {
+    thumbnail.addEventListener('click', (e) => {
         POPUP.classList.remove('hidden');
+        POPUP_IMG.src = e.target.src;
     })
 })
 
