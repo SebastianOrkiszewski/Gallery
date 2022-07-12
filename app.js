896bd57cc227469d8c8e132fa83d68594b63c430
SebastianOrkiszewski <s.orkiszewski@gmail.com>
2022-07-12 11:56:14 +0200

    adding arrow and keydown functionality

--- app.js
+++ app.js
@@ -2,14 +2,85 @@ const THUMBNAIL = document.querySelectorAll('.thumbnail img');
 const POPUP = document.querySelector('.popup');
 const POPUP_CLOSE = document.querySelector('.popup__close');
 const POPUP_IMG = document.querySelector('.popup__img');
+const ARROW_LEFT = document.querySelector('.popup__arrow--left');
+const ARROW_RIGHT = document.querySelector('.popup__arrow--right');
+const POPUP_THUMBNAIL = document.querySelector('.popup__thumbnail img');
 
-THUMBNAIL.forEach((thumbnail) => {
-    thumbnail.addEventListener('click', (e) => {
+let currentImg;
+
+THUMBNAIL.forEach((thumbnail, index) => {
+    const showPopup = (e) => {
         POPUP.classList.remove('hidden');
         POPUP_IMG.src = e.target.src;
-    })
-})
+        currentImg = index;
+        THUMBNAIL.forEach(element => {
+            element.setAttribute("tabindex", -1);
+        });
 
-POPUP_CLOSE.addEventListener('click', () => {
+    }
+
+    thumbnail.addEventListener('click', showPopup);
+
+    thumbnail.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter' || e.keyCode === 13) {
+            showPopup(e);
+        }
+    })
+});
+
+const nextImg = () => {
+    if (currentImg === THUMBNAIL.length - 1) {
+        currentImg = 0;
+    } else {
+        currentImg++;
+    }
+    POPUP_IMG.src = THUMBNAIL[currentImg].src;
+};
+
+const previousImg = () => {
+    if (currentImg === 0) {
+        currentImg = 5;
+    } else {
+        currentImg--;
+    }
+    POPUP_IMG.src = THUMBNAIL[currentImg].src;
+};
+
+const exitPopup = () => {
     POPUP.classList.add('hidden');
+    THUMBNAIL.forEach(element => {
+        element.setAttribute("tabindex", 1);
+    });
+    
+};
+
+
+POPUP_CLOSE.addEventListener('click', exitPopup);
+
+ARROW_RIGHT.addEventListener('click', nextImg);
+
+ARROW_LEFT.addEventListener('click', previousImg);
+
+document.addEventListener('keydown', (e) => {
+    if (!POPUP.classList.contains('hidden')) {
+
+
+        if (e.key === 'ArrowRight' || e.keyCode === 39) {
+            nextImg();
+        }
+
+        if (e.key === 'ArrowLeft' || e.keyCode === 37) {
+            previousImg();
+        }
+
+        if (e.key === 'Escape' || e.keyCode === 27) {
+            exitPopup();
+        }
+    }
+});
+
+POPUP.addEventListener('click', (e) => {
+    if (e.target === POPUP) {
+        exitPopup()
+    }
 })
